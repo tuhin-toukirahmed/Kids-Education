ba5a6b8f394b0a75a7c7cf72d74d9f36d84f76ea
Tuhin-DEK <toukirtuhinfb002@gmail.com>
2025-01-26 17:30:33 +0600

    add preloader

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    // Hide the preloader
+    document.getElementById('preloader').style.display = 'none';
+    
+    // Show the main content
+    document.getElementById('content').style.display = 'block';
+});
